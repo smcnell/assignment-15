7dd14eae9cd680c96a03877c12450036eac3796c
Shannon McNellis <shannonmcnellis@Shannons-MacBook-Pro.local>
2017-02-01 21:18:42 -0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// GO!
+
+var buttonEl= document.querySelector(".mybutton")
+
+buttonEl.addEventListener("click", function(evt){
+  var navEl= document.querySelector(".nav-menu")
+  if (navEl.style.display !=="none"){
+    navEl.style.display ="none"
+  }else {
+    navEl.style.display ="block"
+  }
+
+})
+
+
+
+// TASK 2 -- Select an Icon
+
+// TASK 3 -- Move Item From List to List
+
+// TASK 4 -- Add Guest to List
+
+// TASK 5 -- (Adventure Mode)-- Add + Remove Item From List
