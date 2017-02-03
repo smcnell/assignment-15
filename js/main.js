955a0f7d722a5b659d0a3e1d6b58ebb0c05fd5b6
Shannon McNellis <shannonmcnellis@Shannons-MacBook-Pro.local>
2017-02-02 22:23:30 -0500

    done

--- js/main.js
+++ js/main.js
@@ -22,31 +22,60 @@ buttonEl.addEventListener("click", function(evt){
 
 // TASK 2 -- Select an Icon
 
-var allIcons=document.querySelectorAll(".wish-list")
+var allIcons=document.querySelectorAll(".wish-list div")
 // console.log(allIcons)
 
 forEach( allIcons, function( domElement, index, theArray){
+
   domElement.addEventListener("click", function(evt){
+    // console.log(domElement.className)
     // console.log("Hey")
 
     var domElementOfEvent=evt.target;
     var currentTarget= evt.currentTarget;
-    console.log(currentTarget +"current")
-    console.log(domElementOfEvent + "target")
-
-    var itemNameEl= currentTarget.querySelector(".option")
-    // console.log(itemNameEl)
-    if(itemNameEl.className.indexOf("selected")>=0){
-      itemNameEl.className="option"
-    }else {itemNameEl.className="option" + " selected"
-
-  }console.log(itemNameEl.className)
-
-  }
-  )
+    // console.log(currentTarget +"current")
+    // console.log(domElementOfEvent + "target")
 
 
-})
+console.log(currentTarget.className)
+
+  // var classNameEl= current.className
+
+  if (currentTarget.className.indexOf("selected") >=0){
+    // console.log("SELECTED")
+    currentTarget.className="option"
+  } else {currentTarget.className= "option" + " selected"}
+})})
+
+
+
+
+
+// var allIcons=document.querySelectorAll(".wish-list")
+// // console.log(allIcons)
+//
+// forEach( allIcons, function( domElement, index, theArray){
+//   domElement.addEventListener("click", function(evt){
+//     // console.log("Hey")
+//
+//     var domElementOfEvent=evt.target;
+//     var currentTarget= evt.currentTarget;
+//     console.log(currentTarget +"current")
+//     console.log(domElementOfEvent + "target")
+//
+//     var itemNameEl= currentTarget.querySelector(".option")
+//     // console.log(itemNameEl)
+//     if(itemNameEl.className.indexOf("selected")>=0){
+//       itemNameEl.className="option"
+//     }else {itemNameEl.className="option" + " selected"
+//
+//   }console.log(itemNameEl.className)
+// }
+// )
+//
+//
+// })
+
 
 // TASK 3 -- Increase
 
@@ -89,7 +118,7 @@ forEach( listEl, function( arrayElement, index, theArray){
   arrayElement.addEventListener("click", function(evt){
 
     var targetEvent=evt.target;
-    var currentTargetEvent= evt.currentTarget;
+    // var currentTargetEvent= evt.currentTarget;
 
     // console.log(currentTargetEvent +"current")
     // console.log(targetEvent+ "target")
@@ -98,14 +127,14 @@ forEach( listEl, function( arrayElement, index, theArray){
     var goodListEl= document.querySelector(".good-standing-list")
     var naughtyListEl= document.querySelector(".probation-list")
 
-    console.log(targetEvent.className)
+    // console.log(targetEvent.className)
     if (targetEvent.className.indexOf("listy-eyed")>=0){
       goodListEl.removeChild(targetEvent)
-      naughtyListEl.innerHTML+="<li>"+ targetEvent.innerHTML + "</li>"
+      naughtyListEl.innerHTML+="<li class='bad'>"+ targetEvent.innerHTML + "</li>"
 
     } if (targetEvent.className.indexOf("bad")>=0){
       naughtyListEl.removeChild(targetEvent)
-      goodListEl.innerHTML+="<li>" + targetEvent.innerHTML + "</li>"
+      goodListEl.innerHTML+="<li class='listy-eyed'>" + targetEvent.innerHTML + "</li>"
     }
       // console.log ("Not working sad")
 
@@ -115,3 +144,37 @@ forEach( listEl, function( arrayElement, index, theArray){
 
 
 // TASK 5 -- (Adventure Mode)-- Add + Remove Item From List
+var colorBoxesEl= document.querySelectorAll(".palette span")
+// console.log(colorBoxesEl)
+var textboxEl= document.querySelector(".msg")
+// console.log(textboxEl)
+
+// console.log(textboxEl.className)
+
+
+
+
+forEach( colorBoxesEl, function( oneBoxElement, index, theArray){
+  oneBoxElement.addEventListener("click", function(evt){
+    // console.log(oneBoxElement.className)
+
+    var targetedEvent=evt.target;
+    var currentEventTarget= evt.currentTarget;
+    var style = window.getComputedStyle(oneBoxElement);
+    // console.log(style)
+
+      if (targetedEvent.className==="bg-yellow"){
+      textboxEl.style.backgroundColor= "rgb(244, 208, 63)"
+    } if (targetedEvent.className==="bg-orange"){
+      textboxEl.style.backgroundColor="rgb(235, 151, 78)"
+    } if (targetedEvent.className==="bg-red"){
+    textboxEl.style.backgroundColor="rgb(224, 130, 131)"
+    } if (targetedEvent.className==="bg-green"){
+    textboxEl.style.backgroundColor="rgb(200, 247, 197)"
+    } if (targetedEvent.className==="bg-purple"){
+    textboxEl.style.backgroundColor="rgb(137, 196, 244)"
+  }
+
+}
+)
+})
